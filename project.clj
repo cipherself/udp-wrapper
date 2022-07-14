4b677f4189fdfb2a31ddd5eadfb584454954ca04
Alex Whitt <alex.joseph.whitt@gmail.com>
2022-07-14 15:58:06 -0400

    Update dependencies and add deps.edn for Clojure Deps/CLI (#6)

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/skeuomorf/udp-wrapper"
   :license {:name "MIT public license"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
-  :profiles {:dev {:dependencies [[org.clojure/tools.nrepl "0.2.12"]]
-                   :plugins [[jonase/eastwood "0.2.1"]
-                             [lein-kibit "0.1.2"]
-                             [lein-bikeshed "0.2.0"]]}}
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :profiles {:dev {:dependencies [[org.clojure/tools.nrepl "0.2.13"]]
+                   :plugins [[jonase/eastwood "0.3.6"]
+                             [lein-kibit "0.1.7"]
+                             [lein-bikeshed "0.5.0"]]}}
   :aliases {"checkall" ["do" "check," "kibit," "eastwood," "bikeshed"]})
