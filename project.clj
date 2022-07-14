9c8a23759b6a00621442dda86c39325c2837feb5
Mohamed Attia <mu.attiyah@gmail.com>
2022-07-14 22:11:46 +0200

    Bump up dependencies.

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/muattiyah/udp-wrapper"
   :license {:name "MIT public license"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.nrepl "0.2.13"]]
-                   :plugins [[jonase/eastwood "0.3.6"]
-                             [lein-kibit "0.1.7"]
-                             [lein-bikeshed "0.5.0"]]}}
+                   :plugins [[jonase/eastwood "1.2.4"]
+                             [lein-kibit "0.1.8"]
+                             [lein-bikeshed "0.5.2"]]}}
   :aliases {"checkall" ["do" "check," "kibit," "eastwood," "bikeshed"]})
